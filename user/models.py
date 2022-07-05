13bdec9a3b21a29ef2252354890b4b70d6e5bd3c
Yong Su Jeong <knarrblue@gmail.com>
2022-07-05 09:23:29 +0900

    FEAT: 유저 모델 생성

--- user/models.py
+++ user/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
 
 # Create your models here.
+# User Manager
+class UserManager(BaseUserManager):
+    def create_user(self, email, nickname, password=None):
+        if not email:
+            raise ValueError('이메일은 필수 항목입니다.')
+        if not password:
+            raise ValueError('비밀번호는 필수 항목입니다.')
+        if not nickname:
+            raise ValueError('닉네임은 필수 항목 입니다.')
+
+        user = self.model(
+            email=UserManager.normalize_email(email),
+            nickname=nickname,
+        )
+        user.set_password(password)
+        user.full_clean()
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password, nickname, **extra_fields):
+
+        user = self.model(
+            email=email,
+            nickname=nickname,
+        )
+        user.set_password(password)
+        user.full_clean()
+
+        user.is_admin = True
+        user.is_superuser = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+# User Model
+class User(AbstractBaseUser):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField('이메일', unique=True, max_length=100)
+    nickname = models.CharField('닉네임', max_length=100, unique=True)
+
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'nickname'
+    ]
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.nickname
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    class Meta:
+        db_table = 'users'
